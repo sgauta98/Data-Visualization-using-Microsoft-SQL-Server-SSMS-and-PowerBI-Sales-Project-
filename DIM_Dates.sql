39dda7b1ade50f1df68927626678f54a9fa255c1
sgauta98 <141529900+sgauta98@users.noreply.github.com>
2023-09-21 18:16:55 -0400

    Add files via upload

---
+++ DIM_Dates.sql
@@ -0,0 +1,27 @@
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  --[DayNumberOfWeek], 
+  [EnglishDayNameOfWeek] AS Day, 
+  Left([EnglishDayNameOfWeek], 3) AS DayAbbv, 
+  --[SpanishDayNameOfWeek], 
+  --[FrenchDayNameOfWeek], 
+  --[DayNumberOfMonth], 
+  --[DayNumberOfYear], 
+  --[WeekNumberOfYear], 
+  [EnglishMonthName] AS Month, 
+  Left([EnglishMonthName], 3) AS MonthAbbv, 
+  --[SpanishMonthName], 
+  --[FrenchMonthName], 
+  [MonthNumberOfYear] AS MonthNum, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year 
+  --[CalendarSemester], 
+  --[FiscalQuarter], 
+  --[FiscalYear], 
+  --[FiscalSemester]
+FROM 
+  [AdventureWorksDW2022].[dbo].[DimDate] 
+WHERE 
+  CalendarYear >= 2012
+  
